6656de5185cf185d253826f9970b5e38ce59de5b
lukaslanger99 <lukas.langer99@gmx.de>
2022-01-17 16:37:38 +0100

    top bar update& removed notification count for now

--- html/top-bar.php
+++ html/top-bar.php
@@ -12,62 +12,16 @@
             <div class="top-bar-item">
                 <a href="<?php echo DIR_SYSTEM?>"> TaskBoard </a>
             </div>
+            <div class="button" id="createGroupButton"><p>Create Group</p></div>
+            <div class="button" id="createTaskButton"><p>Create Task</p></div>
+            <a href="<?php echo DIR_SYSTEM ?>php/groups.php"><div class="button"><p>Groups</p></div></a>
+            <a href="<?php echo DIR_SYSTEM ?>php/archive.php"><div class="button"><p>Archive</p></div></a>
             <?php
-                $userVerificationState = $taskBoard->getUserVerificationState($userID);
-                ($userVerificationState) ? $notificationCounter = 0 : $notificationCounter = 1;
-
-                $inviteCounter = $taskBoard->getUserGroupInvitesCount($userID);
-                $notificationCounter += $inviteCounter;
-
-                ($notificationCounter) ? $notificationsHTML = '<span class="button__badge">'.$notificationCounter.'</span>' : $notificationsHTML = '';
-
-                if ($_SESSION['enteredUrl'] != '/taskboard/php/profile.php') {
-                    echo '
-                    <div class="dropdown">
-                        <div class="dropbtn">
-                            <i class="fa fa-user fa-2x" aria-hidden="true"></i>
-                            '.$notificationsHTML.'
-                        </div>
-                        <div class="dropdown-content">
-                            <div class="dropdown-item">
-                                <a href="'.DIR_SYSTEM.'php/profile.php">Settings</a>
-                            </div>
-                            <div>
-                                Nightmode 
-                                <label class="switch">
-                                    <input id="nightmode-checkbox" type="checkbox" '.$nightModeState.'>
-                                    <span class="slider round"></span>
-                                </label>
-                            </div>
-                            <div class="dropdown-item">
-                                <a href="'.DIR_SYSTEM.'php/logout.inc.php">Logout</a>
-                            </div>
-                        </div>
-                    </div>';
-                }
                 if ($userID == 1) {
-                    echo '
-                    <a href="'.DIR_SYSTEM.'php/admin.php">
-                        <div class="button">
-                            Admin
-                        </div>
-                    </a>';
+                    echo '<a href="<?php echo DIR_SYSTEM ?>php/admin.php"><div class="button"><p>Admin</p></div></a>';
                 }
             ?>
-            <a href="<?php echo DIR_SYSTEM . 'php/archive.php'?>">
-            <div class="button">
-                Archive
-            </div>
-            </a>
-            <a href="<?php echo DIR_SYSTEM . 'php/groups.php'?>">
-            <div class="button">
-                Groups
-            </div>
-            </a>
-            <div class="button" id="createTaskButton">
-                Create Task
-            </div>
-            <div class="button" id="createGroupButton">
-                Create Group
+            <div class="dropbtn" onclick="showDropDownPopUp()">
+               <p><i class="fa fa-arrow-down" aria-hidden="true"></i></p>
             </div>
         </div>
