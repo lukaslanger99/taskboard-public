c714836b4e78d12df55510884ca7d2e0867a7600
lukaslanger99 <lukas.langer99@gmx.de>
2022-01-22 01:50:53 +0100

    457 mobile layout for panels and groups

--- html/head.php
+++ html/head.php
@@ -2,6 +2,7 @@
         <html lang="en">
             <head>
                 <title>TaskBoard</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <?php
                 echo '<link rel="stylesheet" type="text/css" href="'.DIR_SYSTEM.'css/stylesheet-properties.css">';
                 if ($_SESSION['userID'] && $taskBoard->getNightmodeEnabled($_SESSION['userID'])) {
